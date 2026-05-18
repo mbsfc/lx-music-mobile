25a0b29ca4d0e038bc4201b38a0f32342191f416
WIN-KN3VVTK6MKA\EDY <211977992@qq.com>
2026-05-18 15:22:21 +0800

    fix: restore rn bundle commands in config

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,15 @@
 ﻿const ios = require('@react-native-community/cli-platform-ios')
 const android = require('@react-native-community/cli-platform-android')
+const communityCliPlugin = require('@react-native/community-cli-plugin')
 
 module.exports = {
+  commands: [
+    ...ios.commands,
+    ...android.commands,
+    communityCliPlugin.bundleCommand,
+    communityCliPlugin.ramBundleCommand,
+    communityCliPlugin.startCommand,
+  ].filter(Boolean),
   platforms: {
     ios: {
       projectConfig: ios.projectConfig,
